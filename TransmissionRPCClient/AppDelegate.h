21bd201242a16f196e9b8e2cd94a060b66a8dd2b
Alexey Chechetkin <alcheck@Alexeys-iMac.local>
2015-06-23 17:34:14 +0300

    Initial Commit

---
+++ TransmissionRPCClient/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TransmissionRPCClient
+//
+//  Created by Alexey Chechetkin on 23.06.15.
+//  Copyright (c) 2015 Alexey Chechetkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TransmissionRPCClient/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TransmissionRPCClient
+//
+//  Created by Alexey Chechetkin on 23.06.15.
+//  Copyright (c) 2015 Alexey Chechetkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
